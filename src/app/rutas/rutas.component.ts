af01c22eb2f9de6df975892608e5d10f71b9013a
andresborbor <adborbor@espol.edu.ec>
2023-08-27 17:37:34 -0500

    componente ruta terminado

--- src/app/rutas/rutas.component.ts
+++ src/app/rutas/rutas.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Ruta } from '../interfaces/ruta';
 
 @Component({
   selector: 'app-rutas',
@@ -6,5 +7,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./rutas.component.css']
 })
 export class RutasComponent {
-  
+  likeImage = '/assets/like.png';
+  image = '/assets/dislike.png';
+  data:Ruta[] = [{
+    nombre:'ruta1',
+    tiempo: '25min',
+    valoracion:4.0,
+    dinero: 100,
+    likes:20,
+    no_recomendado: 15,
+  },{
+    nombre:'ruta2',
+    tiempo: '25min',
+    valoracion:4.0,
+    dinero: 250,
+    likes:20,
+    no_recomendado: 7,
+  },{
+    nombre:'ruta3',
+    tiempo: '25min',
+    valoracion:4.0,
+    dinero: 400,
+    likes:20,
+    no_recomendado: 5,
+  }]
+
+
+  like(){
+      console.log("like");
+    
+  }
+
+  dislike(){
+    console.log('dislike');
+  }
 }
