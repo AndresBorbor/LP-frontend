2fcd66af962b5f5c931eb724823513246db62e76
andresborbor <adborbor@espol.edu.ec>
2023-09-02 10:31:48 -0500

    ejemplos de rutas

--- src/app/rutas/rutas.component.ts
+++ src/app/rutas/rutas.component.ts
@@ -10,26 +10,26 @@ export class RutasComponent {
   likeImage = '/assets/like.png';
   image = '/assets/dislike.png';
   data:Ruta[] = [{
-    nombre:'ruta1',
+    nombre:'Ruta Guayarte',
     tiempo: '25min',
     valoracion:4.0,
     dinero: 100,
     likes:20,
     no_recomendado: 15,
   },{
-    nombre:'ruta2',
+    nombre:'Ruta ceibos',
     tiempo: '25min',
     valoracion:4.0,
     dinero: 250,
-    likes:20,
+    likes:32,
     no_recomendado: 7,
   },{
-    nombre:'ruta3',
-    tiempo: '25min',
+    nombre:'Ruta Urdesa',
+    tiempo: '30min',
     valoracion:4.0,
     dinero: 400,
-    likes:20,
-    no_recomendado: 5,
+    likes:40,
+    no_recomendado: 14,
   }]
 
 
